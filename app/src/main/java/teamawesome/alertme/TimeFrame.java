f60f0a79f01f12b722bc821fab042d64ef6417fa
Michelle Lawrence <michelle.lawrence@utexas.edu>
2015-03-29 16:09:14 -0500

    Saved states for time frame activity

--- app/src/main/java/teamawesome/alertme/TimeFrame.java
+++ app/src/main/java/teamawesome/alertme/TimeFrame.java
@@ -1,5 +1,6 @@
 package teamawesome.alertme;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
@@ -9,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,17 +24,20 @@ public class TimeFrame extends ActionBarActivity {
     private boolean[] weekdays = {false, false, false, false, false, false, false};
 
     //time frame checkboxes
-    private int numberOfTimeFrameBoxesChecked = 0;
     private CheckBox twelveHour, twentyFourHour;
-    private int timeFrame = 0;
+    private int timeFrame = 12; //default twelveHour
 
     //time of day check boxes
-    private int numberOfTimeDayBoxesChecked = 0;
     private CheckBox am, pm;
     private boolean inMorning = false;
 
     //Alert time seekbar
     private int changedProgress = 0;
+    private SeekBar alertTime;
+
+    //Sounds
+    private CheckBox vibrate;
+    private Switch sound;
 
     //to restore settings
     private SharedPreferences mPrefs;
@@ -50,8 +55,9 @@ public class TimeFrame extends ActionBarActivity {
         } else {
             throw new AssertionError("TimeFrame: Failed to access AlarmDataSingleton list at " + alarmIndex);
         }
+
         //Seekbar
-        SeekBar alertTime = (SeekBar) findViewById(R.id.seekBar4);
+        alertTime = (SeekBar) findViewById(R.id.seekBar4);
         alertTime.setOnSeekBarChangeListener(alertTimeListener);
 
         addListenerWeekday();
@@ -62,7 +68,11 @@ public class TimeFrame extends ActionBarActivity {
 
         addListenerAm();
         addListenerPm();
+
+        addListenerSound();
+        addListenerVibrate();
     }
+
 
 
     @Override
@@ -87,13 +97,131 @@ public class TimeFrame extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("seekBar", changedProgress);
+
+        outState.putBooleanArray("weekdays", weekdays);
+        outState.putBoolean("weekday", weekday.isChecked());
+        outState.putBoolean("weekend", weekend.isChecked());
+
+        outState.putBoolean("twelveHour", twelveHour.isChecked());
+        outState.putBoolean("twentyFourHour", twentyFourHour.isChecked());
+
+        outState.putBoolean("am", am.isChecked());
+        outState.putBoolean("pm", pm.isChecked());
+
+        outState.putBoolean("sound", sound.isChecked());
+        outState.putBoolean("vibrate", vibrate.isChecked());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+
+        changedProgress = savedInstanceState.getInt("seekBar");
+        TextView displayValue = (TextView) findViewById(R.id.seekBarTextView);
+        displayValue.setText("" + changedProgress);
+        alertTime.setProgress(changedProgress);
+
+        weekdays = savedInstanceState.getBooleanArray("weekdays");
+
+        //weekdays checkboxes
+        weekday.setChecked(savedInstanceState.getBoolean("weekday"));
+        weekend.setChecked(savedInstanceState.getBoolean("weekend"));
+
+        //timeframe checkboxes
+        twelveHour.setChecked(savedInstanceState.getBoolean("twelveHour"));
+        twentyFourHour.setChecked(savedInstanceState.getBoolean("twentyFourHour"));
+        if(twelveHour.isChecked()){timeFrame = 12;}
+        else {timeFrame = 24;}
+
+        //am/pm checkboxes
+        am.setChecked(savedInstanceState.getBoolean("am"));
+        pm.setChecked(savedInstanceState.getBoolean("pm"));
+        if (am.isChecked()){inMorning = true;}
+        else {inMorning = false;}
+
+        sound.setChecked(savedInstanceState.getBoolean("sound"));
+        vibrate.setChecked(savedInstanceState.getBoolean("vibrate"));
+
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //check boxes
+        save(weekday.isChecked(), "weekday");
+        save(weekend.isChecked(), "weekend");
+        save(twelveHour.isChecked(), "twelveHour");
+        save(twentyFourHour.isChecked(), "twentyFourHour");
+        save(am.isChecked(), "am");
+        save(pm.isChecked(), "pm");
+        save(vibrate.isChecked(), "vibrate");
+        //switch
+        save(sound.isChecked(), "sound");
+
+        //seekbar
+        mPrefs = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mPrefs.edit();
+        editor.putInt("seekBar", changedProgress);
+
+        editor.apply();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //check boxes
+        weekday.setChecked(load("weekday"));
+        weekend.setChecked(load("weekend"));
+
+        //time frame
+        twelveHour.setChecked(load("twelveHour"));
+        twentyFourHour.setChecked(load("twentyFourHour"));
+        if (twelveHour.isChecked()){timeFrame = 12;}
+        else {timeFrame = 24;}
+
+        //am/pm checkboxes
+        am.setChecked(load("am"));
+        pm.setChecked(load("pm"));
+        if (am.isChecked()){inMorning = true;}
+        else{inMorning = false;}
+
+        //sounds
+        vibrate.setChecked(load("vibrate"));
+        sound.setChecked(load("sound"));
+
+        //seekbar
+        changedProgress = mPrefs.getInt("seekBar", 0);
+        TextView displayValue = (TextView) findViewById(R.id.seekBarTextView);
+        displayValue.setText("" + changedProgress);
+        alertTime.setProgress(changedProgress);
+
+    }
+
+    private void save(final boolean isChecked, String saveName) {
+        mPrefs = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mPrefs.edit();
+        editor.putBoolean(saveName, isChecked);
+        editor.apply();
+    }
+
+    private boolean load(String saveName) {
+        mPrefs = getPreferences(Context.MODE_PRIVATE);
+        return mPrefs.getBoolean(saveName, false);
+    }
+
     //Alert Time seekbar
     private SeekBar.OnSeekBarChangeListener alertTimeListener = new SeekBar.OnSeekBarChangeListener() {
 
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
             changedProgress = progress;
             TextView displayValue = (TextView) findViewById(R.id.seekBarTextView);
-            displayValue.setText("" + progress);
+            displayValue.setText("" + changedProgress);
         }
 
         public void onStartTrackingTouch(SeekBar seekBar) {
@@ -156,17 +284,9 @@ public class TimeFrame extends ActionBarActivity {
 
             @Override
             public void onClick(View v) {
-                if (((CheckBox) v).isChecked() && numberOfTimeFrameBoxesChecked >= 1) {
+                if (((CheckBox) v).isChecked() && twentyFourHour.isChecked()) {
                     twelveHour.setChecked(false);
                 } else {
-                    // the checkbox either got unchecked
-                    // or there are less than 2 other checkboxes checked
-                    // change your counter accordingly
-                    if (((CheckBox) v).isChecked()) {
-                        numberOfTimeFrameBoxesChecked++;
-                    } else {
-                        numberOfTimeFrameBoxesChecked--;
-                    }
                     timeFrame = 12;
                 }
             }
@@ -182,17 +302,9 @@ public class TimeFrame extends ActionBarActivity {
 
             @Override
             public void onClick(View v) {
-                if (((CheckBox) v).isChecked() && numberOfTimeFrameBoxesChecked >= 1) {
+                if (((CheckBox) v).isChecked() && twelveHour.isChecked()) {
                     twentyFourHour.setChecked(false);
                 } else {
-                    // the checkbox either got unchecked
-                    // or there are less than 2 other checkboxes checked
-                    // change your counter accordingly
-                    if (((CheckBox) v).isChecked()) {
-                        numberOfTimeFrameBoxesChecked++;
-                    } else {
-                        numberOfTimeFrameBoxesChecked--;
-                    }
                     timeFrame = 24;
                 }
             }
@@ -208,17 +320,9 @@ public class TimeFrame extends ActionBarActivity {
 
             @Override
             public void onClick(View v) {
-                if (((CheckBox) v).isChecked() && numberOfTimeDayBoxesChecked >= 1) {
+                if (((CheckBox) v).isChecked() && pm.isChecked()) {
                     am.setChecked(false);
                 } else {
-                    // the checkbox either got unchecked
-                    // or there are less than 2 other checkboxes checked
-                    // change your counter accordingly
-                    if (((CheckBox) v).isChecked()) {
-                        numberOfTimeDayBoxesChecked++;
-                    } else {
-                        numberOfTimeDayBoxesChecked--;
-                    }
                     inMorning = true;
                 }
             }
@@ -234,36 +338,56 @@ public class TimeFrame extends ActionBarActivity {
 
             @Override
             public void onClick(View v) {
-                if (((CheckBox) v).isChecked() && numberOfTimeDayBoxesChecked >= 1) {
+                if (((CheckBox) v).isChecked() && am.isChecked()) {
                     pm.setChecked(false);
                 } else {
-                    // the checkbox either got unchecked
-                    // or there are less than 2 other checkboxes checked
-                    // change your counter accordingly
-                    if (((CheckBox) v).isChecked()) {
-                        numberOfTimeDayBoxesChecked++;
-                    } else {
-                        numberOfTimeDayBoxesChecked--;
-                    }
                     inMorning = false;
                 }
             }
         });
     }//end pm checkbox
 
-    public void soundToggle(View v){
-        currentAlarm.toggleSound();
-    }
+    //Sound switch
+    public void addListenerSound() {
 
-    public void vibrateToggle(View v){
-        currentAlarm.toggleVibrate();
-    }
+        sound = (Switch) findViewById(R.id.switch1);
 
-    public void toAlarmList(View view){
+        sound.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (((Switch) v).isChecked()) {
+                   currentAlarm.toggleSound();
+                }
+            }
+        });
+    }//end sound switch
+
+    //vibrate Checkbox
+    public void addListenerVibrate() {
+
+        vibrate = (CheckBox) findViewById(R.id.checkBox11);
+
+        vibrate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (((CheckBox) v).isChecked()) {
+                    currentAlarm.toggleVibrate();
+                }
+            }
+        });
+    }//end vibrate checkbox
+
+    private void saveInfo (){
         currentAlarm.setAlertTime(changedProgress);
         currentAlarm.setDaysSelected(weekdays);
         currentAlarm.setTimeFrame(timeFrame);
         currentAlarm.setAmPm(inMorning);
+    }
+
+    public void toAlarmList(View view){
+        saveInfo();
 
         Intent intent = new Intent(this, AlarmList.class);
         startActivity(intent);
