2f514ee93118806ee9d2d656c87dcb351563da50
Clint Wu <wuclint@utexas.edu>
2015-04-02 17:51:28 -0500

    Added alarm class (untested)

---
+++ app/src/main/java/teamawesome/alertme/AlarmBroadcastReceiver.java
@@ -0,0 +1,40 @@
+package teamawesome.alertme;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+import android.widget.Toast;
+
+
+public class AlarmBroadcastReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "");
+        wakeLock.acquire();
+
+        // Put here YOUR code.
+        Toast.makeText(context, "Alarm !!!!!!!!!!", Toast.LENGTH_LONG).show();
+
+        wakeLock.release();
+    }
+
+    public void setAlarm(Context context) {
+        AlarmManager am=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(context, AlarmBroadcastReceiver.class);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * 10, pi); // Millisec * Second * Minute
+    }
+
+    public void cancelAlarm(Context context) {
+        Intent intent = new Intent(context, AlarmBroadcastReceiver.class);
+        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(sender);
+    }
+
+}
