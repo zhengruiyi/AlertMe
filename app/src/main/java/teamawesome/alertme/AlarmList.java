e1699ed889ba20aecc9ee401cd1b3192963bf948
Michelle Lawrence <michelle.lawrence@utexas.edu>
2015-04-09 17:35:36 -0500

    removed non-functional Settings in action bar

--- app/src/main/java/teamawesome/alertme/AlarmList.java
+++ app/src/main/java/teamawesome/alertme/AlarmList.java
@@ -46,29 +46,6 @@ public class AlarmList extends ActionBarActivity {
         task.execute(new String[]{city});
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_alarm_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void toConditions(View view) {
         Intent intent = new Intent(this, Conditions.class);
         intent.putExtra("alarmIndex", 0);
--- app/src/main/java/teamawesome/alertme/TimeFrame.java
+++ app/src/main/java/teamawesome/alertme/TimeFrame.java
@@ -74,29 +74,6 @@ public class TimeFrame extends ActionBarActivity {
     }
 
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_time_frame, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override
     protected void onSaveInstanceState(Bundle outState){
         super.onSaveInstanceState(outState);
