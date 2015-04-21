47bf0dedb7aab348c1b1fd13dc2c7df1d1f101d7
Clint Wu <wuclint@utexas.edu>
2015-04-21 04:43:27 -0500

    MetadataSingleton: Example and way to save all weather forecast data

--- app/src/main/java/teamawesome/alertme/AlarmList.java
+++ app/src/main/java/teamawesome/alertme/AlarmList.java
@@ -139,9 +139,9 @@ public class AlarmList extends ActionBarActivity {
             AlertMeMetadataSingleton.getInstance().setWeather(weather, AlarmList.this);
 
             SharedPreferences currentWeatherData = getSharedPreferences("weather_data", MODE_PRIVATE);
-            dataTemp.setText("Temperature: " + currentWeatherData.getFloat("tomorrowMinTemperature", -1.0f) + "F");
+            dataTemp.setText("Temperature: " + currentWeatherData.getFloat("tomorrowMinTemperatureF", -1.0f) + "F");
             dataRain.setText("Precipitation: " + currentWeatherData.getFloat("tomorrowPrecipitationChance", -1.0f) + "%");
-            dataWindSpeed.setText("Wind Speed: " + currentWeatherData.getFloat("tomorrowWindSpeed", -1.0f) + "mph");
+            dataWindSpeed.setText("Wind Speed: " + currentWeatherData.getFloat("tomorrowWindSpeedMph", -1.0f) + "mph");
         }
     }
 }
